9d7837122df9406da51c0e0aad98edfa313ae579
tapphughesn <nicholas.tapphughes@gmail.com>
2026-04-27 11:38:18 -0400

    rename resume after compilation

--- nickcv-1page/build_and_watch_document.sh
+++ nickcv-1page/build_and_watch_document.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
+DATE=$(date +"%B_%d_%Y")
 latexmk -pvc -lualatex main.tex
+cp main.pdf "Tapp-Hughes_Nicholas_Resume_${DATE}.pdf"
--- nickcv-1page/build_document.sh
+++ nickcv-1page/build_document.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
+DATE=$(date +"%B_%d_%Y")
 latexmk -lualatex main.tex
+cp main.pdf "Tapp-Hughes_Nicholas_Resume_${DATE}.pdf"
